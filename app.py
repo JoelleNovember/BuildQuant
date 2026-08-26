780fed35a3ed04c86edb67c926364bbff2769edf
Joelle November <novemberjoelle@gmail.com>
2026-08-26 13:55:18 +0200

    Create BuildQunt GUI

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "BuildQuant",
+    page_icon = "🏗️"
+)
+
+
+st.title("🏗️ BuildQuant")
+
+st.subheader("Residential Quantity Estimation System")
+
+st.write(
+    "BuildQaunt collects proposed dwelling infromation"
+    "and transforms it into construction quality estimates."
+)
+
+st.info("Day 1: BuildQuant project successfully started!")
