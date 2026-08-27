07f297531ebdaab45ee1db879f2055c189c887f2
Joelle November <jonovcpt025@student.wethinkcode.co.za>
2026-08-27 11:42:03 +0200

    Create Streamlit header Project Information

--- app.py
+++ app.py
@@ -14,3 +14,34 @@ st.title("🏗️ BuildQuant")
 st.title("Residential Quantity System")
 
 st.divider()
+
+
+#PROJECT INFORMATION
+st.header("📋 Project Information")
+
+project_name = st.text_input(
+    "Project Name",
+    placeholder="e.g Doe Residence"
+)
+
+project_number = st.text_input(
+    "Project Number",
+    placeholder="e.g PRJ_001"
+)
+
+location = st.text_input(
+    "Location",
+    placeholder="e.g Cape Town"
+)
+
+building_type = st.selectbox(
+    "Building Type",
+    [
+        "Single-storey dwelling",
+        "Double-storey dewlling"
+    ]
+)
+
+st.divider()
+
+
