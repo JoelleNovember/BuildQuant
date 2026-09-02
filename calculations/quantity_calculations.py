04895f728ee3617415c9520f09a9267ca52552f0
Joelle November <jonovcpt025@student.wethinkcode.co.za>
2026-09-02 09:47:10 +0200

    feat(calculations): implement the dwelling dimensions formulas

--- calculations/quantity_calculations.py
+++ calculations/quantity_calculations.py
@@ -3,6 +3,45 @@
 def calculate_flooor_area(length, width):
     """
     Calculate the floor area of a rectangular dwelling.
+
+    Formula: length × width
     """
 
     return length * width
+
+
+def calculate_perimeter(length, width):
+    """
+    Calculate the perimeter of a rectangular dwelling.
+
+    Formula: 2 × (length + width)
+    """
+    return 2 * (length + width)
+
+
+def calculate_gross_wall_area(perimeter, wall_height):
+    """
+    Calculate the gross external wall area.
+
+    Formula:
+    perimeter x wall height
+    """
+    return perimeter * wall_height
+
+
+def calculate_opening_area(width, height, quantity):
+    """
+    Calculate the total area of amn opening
+
+    Formula:
+    width x height x quantity
+
+    """
+    return width * height * quantity
+
+def caluclate_net_wall_area(gross_wall_area, door_area, window_area):
+    """
+    Calculate the wall area after deducting doors and windows. 
+    Formula: gross wall area - door area - window area
+    """
+    return gross_wall_area - door_area - window_area 
