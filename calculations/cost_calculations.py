9126aa0aab8bd1ef6b17645f2aec337d75376309
Joelle November <novemberjoelle@gmail.com>
2026-09-15 22:03:03 +0200

    calculations: Implemented cost calculations.py

---
+++ calculations/cost_calculations.py
@@ -0,0 +1,18 @@
+def calculate_material_cost(quantity, rate):
+    """
+    Calculate the esitmated cost of a material
+
+    Formula:
+    quantity X rate
+    """
+
+    return quantity * rate
+
+
+ def calculate_total_cost(material_costs):
+    """
+    Calculate the total esitmate cost.
+
+    material_costs should be a lisy of costs.
+    """
+    return sum(material_costs)
